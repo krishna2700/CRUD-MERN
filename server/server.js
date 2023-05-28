ea9ee41cb79e6e66d15b7eff9154bdafdf263bef
KrishnaRuparelia <krishna.ruparelia@ken42.com>
2023-05-28 15:00:06 +0530

    backend setup completed (11:00)

---
+++ server/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const app = express();
+const port = 5000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req, res) => res.send("Hello From Express"));
+
+app.listen(port, () =>
+  console.log(`Server is listening on port: http://localhost:${port}`)
+);
