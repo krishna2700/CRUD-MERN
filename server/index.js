b11f177b4ababb55f30f77b28426ca9d77ab6065
KrishnaRuparelia <krishna.ruparelia@ken42.com>
2023-06-18 01:35:33 +0530

    start of creating add api

--- server/index.js
+++ server/index.js
@@ -1,11 +1,23 @@
 import express from "express";
 import connection from "./database/db.js";
+import dotenv from "dotenv";
+import Routes from "./routes/routes.js";
+import cors from "cors";
 
 const app = express();
 
+dotenv.config();
+
+app.use(cors());
+
+app.use("/", Routes);
+
 const PORT = 8000;
 
-connection();
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+
+connection(username, password);
 
 app.listen(PORT, () =>
   console.log(`Server is running successfully on PORT ${PORT} `)
